e03ab87efdcd5fcafa3c5270bb35f4089128063c
hinata <kd1411823@st.kobedenshi.ac.jp>
2026-03-22 01:51:29 +0900

    ゲームの進行システムクラス作成

---
+++ Source/ProjectNull/System/Subsystem/WorldSubsystem/GameProgressSubsystem/GameProgressSubsystem.h
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "GameProgressSubsystem.generated.h"
+
+/// <summary>
+/// ゲームの進行管理クラス
+/// </summary>
+UCLASS()
+class PROJECTNULL_API UGameProgressSubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+	
+public:
+
+	/// <summary>
+	/// 敵を倒した際に倒した敵数を加算
+	/// </summary>
+	/// <param name="Value">加算したい敵数</param>
+	void AddKillCount(int32 Value = 1);
+
+	int32 GetKillCount() const { return KillCount; }
+
+	int32 GetPhase() const { return Phase; }
+
+	/// <summary>
+	/// フェーズの境界を定義する倒した敵数閾値
+	/// </summary>
+	UPROPERTY(EditAnywhere)
+	TArray<int32> PhaseThresholds;
+
+private:
+
+	/// <summary>
+	/// 敵を倒した数
+	/// </summary>
+	int32 KillCount = 0;
+
+	/// <summary>
+	///	ゲームのフェーズ数
+	/// </summary>
+	int32 Phase = 0;
+
+	/// <summary>
+	/// ゲームのフェーズ更新
+	/// ※毎フレーム呼ぶメソッドではない
+	/// </summary>
+	void UpdatePhase();
+};
