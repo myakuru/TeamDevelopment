8259bb653ec8beb144cb01b89f0ed5c4e1775d9d
myakuru <sh1016n@icloud.com>
2026-04-10 13:51:43 +0900

    Revert "Merge branch 'Hinata/feature'"
    This reverts commit cda91a35e09ab4e8467e0b061387e9717789f992, reversing
    changes made to b4adce5ff521f39d169567caa4db217afd6104df.

--- Source/ProjectNull/Actor/Effect/FloatingWeaponEffect/State/FloatingWeaponStateBase.h
+++ Source/ProjectNull/Actor/Effect/FloatingWeaponEffect/State/FloatingWeaponStateBase.h
@@ -5,9 +5,6 @@
 #include "UObject/Object.h"
 #include "FloatingWeaponStateBase.generated.h"
 
-//　浮遊武器クラス
-class UFloatingWeaponEffect;
-
 /// <summary>
 //　浮遊武器の状態基底クラス
 /// </summary>
@@ -22,31 +19,6 @@ public:
 
 public:
 
-
-
-	/// <summary>
-	/// 更新
-	/// </summary>
-	/// <param name="DeltaTime">デルタタイム</param>
-	virtual void Update(AActor* OwnerActor, float DeltaTime);
-
-	//　セッター
-	inline void SetOnwer(UFloatingWeaponEffect* SetOwner) { Owner = SetOwner; }
-
-protected:
-
-	UPROPERTY()
-	UFloatingWeaponEffect* Owner;
-
-	UPROPERTY(EditAnywhere)
-	FTransform Transform;
-
-	//　エフェクトの半径オフセット
-	UPROPERTY(EditAnywhere)
-	float RadiusOffset;
-
-	//　エフェクトの回転オフセット
-	UPROPERTY(EditAnywhere)
-	FRotator RotatorOffset;
+	virtual void Update(float DeltaTime) { return; }
 
 };
