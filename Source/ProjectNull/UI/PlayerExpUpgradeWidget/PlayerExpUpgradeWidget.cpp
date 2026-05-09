569c2159b7982e7269c6234cb9e903406c051683
myakuru-todo <myakuru72@gmailcom>
2026-05-10 01:36:10 +0900

    デスクトップ移行用

--- Source/ProjectNull/UI/PlayerExpUpgradeWidget/PlayerExpUpgradeWidget.cpp
+++ Source/ProjectNull/UI/PlayerExpUpgradeWidget/PlayerExpUpgradeWidget.cpp
@@ -2,4 +2,27 @@
 
 
 #include "PlayerExpUpgradeWidget.h"
+#include <ProjectNull/GameInstance/SuperGameInstance.h>
 
+void UPlayerExpUpgradeWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	GameInstance = Cast<USuperGameInstance>(GetGameInstance());
+	if (GameInstance)
+	{
+		PlayerRuntimeData = GameInstance->GetPlayerRuntimeData();
+	}
+}
+
+void UPlayerExpUpgradeWidget::OpenWidget()
+{
+	SetVisibility(ESlateVisibility::Visible);
+	SetIsEnabled(true);
+}
+
+void UPlayerExpUpgradeWidget::CloseWidget()
+{
+	SetVisibility(ESlateVisibility::Hidden);
+	SetIsEnabled(false);
+}
