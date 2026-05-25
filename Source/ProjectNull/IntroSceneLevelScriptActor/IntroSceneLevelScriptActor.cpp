72030006b4b58a17fafb80aebd3915a0bd065ee7
myakuru-todo <myakuru72@gmailcom>
2026-05-25 10:53:24 +0900

    一旦イントロシーンのクラスだけ追加させた

---
+++ Source/ProjectNull/IntroSceneLevelScriptActor/IntroSceneLevelScriptActor.cpp
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "IntroSceneLevelScriptActor.h"
+#include "Kismet/GameplayStatics.h"
+#include "ProjectNull/Actor/Character/CombatCharacterBase/Player/PlayerBase.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+void AIntroSceneLevelScriptActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// プレイヤーのカメラを降下シーン用の位置に移動させる
+	PlayerIntroSceneStart();
+}
+
+void AIntroSceneLevelScriptActor::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+}
+
+void AIntroSceneLevelScriptActor::PlayerIntroSceneStart()
+{
+	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(this, 0);
+
+	Player = Cast<APlayerBase>(PlayerPawn);
+
+	if (Player)
+	{
+		SpringArmComponent = Player->GetSpringArmComponent();
+		CameraComponent = Player->GetCameraComponent();
+
+		SpringArmComponent->SetRelativeLocation(FVector(-300.0f, 0.0f, 300.0f));
+		CameraComponent->AddLocalRotation(FRotator(-180.0f, 0.0f, 0.0f));
+	}
+}
