a4dd6434cdd39d3082bcc0c0c2904eb13fd3eaf8
kd1411823 <kd1411823@st.kobedenshi.ac.jp>
2026-05-25 13:54:36 +0900

    マージ

---
+++ Source/ProjectNull/Actor/Projectile/ProjectileBase.cpp
@@ -0,0 +1,21 @@
+﻿
+#include "ProjectileBase.h"
+
+AProjectileBase::AProjectileBase()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AProjectileBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AProjectileBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
--- Source/ProjectNull/System/Gear/LaserGear/State/LaserGearState_Lv1/LaserGearState_Lv1.h
+++ Source/ProjectNull/System/Gear/LaserGear/State/LaserGearState_Lv1/LaserGearState_Lv1.h
@@ -2,12 +2,12 @@
 #pragma once
 
 #include "CoreMinimal.h"
+
 #include <ProjectNull/System/Gear/LaserGear/State/LaserGearStateBase.h>
+
 #include "LaserGearState_Lv1.generated.h"
 
-/**
- * 
- */
+/** レーザーギアの1レベル状態クラス */
 UCLASS(Blueprintable, EditInlineNew)
 class PROJECTNULL_API ULaserGearState_Lv1 : public ULaserGearStateBase
 {
