06772381fb1810952a7ec60af5c7b6970a22d8cc
HaruHaruru0324 <doesplayer0324@icloud.com>
2026-06-10 16:35:44 +0900

    宝箱のギア抽選機能追加
    宝箱にギアの抽選機能を追加
    ・DA_StageDataにステージごとに排出されるギアの種類とそれぞれの確率を記録
    ・DA_StageDataはStageManagerが所持
    ・宝箱にギアの名前があればそのギアを排出、なければデータに則りランダム抽選する。
    
    宝箱から排出されたギアはStageManagerで一時的に記録され、ステージの開始時とクリア時に消去される。

--- Source/ProjectNull/Stage/Manager/StageManager.cpp
+++ Source/ProjectNull/Stage/Manager/StageManager.cpp
@@ -6,9 +6,36 @@
 #include <ProjectNull/SaveGame/MySaveGame.h>
 #include<ProjectNull/SaveGame/StageProgressData.h>
 
+#include <ProjectNull/UI/OutGame/StageDataAsset/StageDataAsset.h>
+
 void UStageManager::LoadFromSaveData(UMySaveGame* inSaveGame)
 {
 	StageProgressList = &inSaveGame->StageProgressList;
+
+	// ステージ数取得
+	if (StageDataAsset)
+	{
+		const int32 StageCount = StageDataAsset->StageData.Num();
+
+		if (StageProgressList->Num() < StageCount)
+		{
+			StageProgressList->SetNum(StageCount);
+		}
+
+		// 最初のステージは解放しておく
+		if (StageCount > 0) {
+
+			//ステージのクリア状況を初期化(デバッグ用)
+			//for(int i = 0; i < StageCount; i++)
+			//{
+			//	CurrentSaveData->StageProgressList[i].MissionClears.SetNum(3);
+
+			//	CurrentSaveData->StageProgressList[i].bUnlocked = false;
+			//}
+
+			(*StageProgressList)[0].bUnlocked = true;
+		}
+	}
 }
 
 void UStageManager::SaveToData(UMySaveGame* inSaveGame)
@@ -19,6 +46,9 @@ void UStageManager::SaveToData(UMySaveGame* inSaveGame)
 void UStageManager::StageStart(int32 inNowStageIndex)
 {
 	NowStageIndex = inNowStageIndex;
+
+	//取得ギアのリセット
+	AcquiredWeapons.Reset();
 }
 
 void UStageManager::StageClear()
@@ -33,6 +63,9 @@ void UStageManager::StageClear()
 
 	//セーブ
 	GetWorld()->GetGameInstance<USuperGameInstance>()->SaveGameData();
+
+	//取得ギアのリセット
+	AcquiredWeapons.Reset();
 
 	UGameplayStatics::OpenLevel(this, "StageSelectLevel");
 }
