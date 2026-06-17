72b28928e4fb57a8e18c3454a358f13dd79835a9
myakuru-todo <myakuru72@gmailcom>
2026-06-17 14:49:01 +0900

    ごぺんなさい

---
+++ Source/ProjectNull/IntroSceneLevelScriptActor/IntroSceneLevelScriptActor.cpp
@@ -0,0 +1,13 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "IntroSceneLevelScriptActor.h"
+#include "Kismet/GameplayStatics.h"
+#include "ProjectNull/Actor/Character/CombatCharacterBase/Player/PlayerBase.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+void AIntroSceneLevelScriptActor::BeginPlay()
+{
+	Super::BeginPlay();
+}
