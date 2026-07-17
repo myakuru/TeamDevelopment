090b1cb0b7797793f47409cb9b5e465e4169e720
Ebi <kd1404863@st.kobedenshi.ac.jp>
2026-07-17 13:33:01 +0900

    連続HITするようになった！、、はず

--- Source/ProjectNull/System/Combat/Attack/CollisionAttack/CollisionAttack.cpp
+++ Source/ProjectNull/System/Combat/Attack/CollisionAttack/CollisionAttack.cpp
@@ -2,8 +2,9 @@
 #include <ProjectNull\System\Interface\CharacterInterface\CharacterInterface.h>
 
 UCollisionAttack::UCollisionAttack()
-	:	Duration(1.f)
-	,	JustExecuteTime(0.f)
+	: MaxDuration(0.f)
+	  , JustExecuteTime(0.f)
+	  , BlinkingElement(FBlinkingHitElemental())
 {
 }
 
@@ -14,24 +15,52 @@ void UCollisionAttack::Update(const float InDeltaTime)
 
 	UAttackBase::Update(InDeltaTime);
 
+	const float CurrentTime = GetWorld()->GetTimeSeconds();
+
 	// 発動した瞬間から有効時間分経過していたら終了
-	if (GetWorld()->GetTimeSeconds() > Duration + JustExecuteTime)
+	if (CurrentTime > MaxDuration + JustExecuteTime)
 	{
 		Cancel();
 		return;
+	}
+
+	// 点滅判定がfalseでHIT配列がEmptyでないなら一度リセット
+	if (!IsHitCheckActiveNow(CurrentTime-JustExecuteTime)&&!GetHitActors().IsEmpty())
+	{
+		ResetAllActors();
+		UE_LOG(LogTemp,Warning,TEXT("ArraySize  : %d"),GetHitActors().Num());
 	}
 }
 
 void UCollisionAttack::Execute(const FVector& InTargetLocation)
 {
-	SetIsActive(true);								// 攻撃有効化
-	SetCanExecute(false);							// 攻撃実行不可にする
-	JustExecuteTime = GetWorld()->GetTimeSeconds();	// 「発動した時間」として現在の時間を保存
+	SetIsActive(true); // 攻撃有効化
+	SetCanExecute(false); // 攻撃実行不可にする
+	JustExecuteTime = GetWorld()->GetTimeSeconds(); // 「発動した時間」として現在の時間を保存
+	
+	// 既に持っているアクター配列をリセット
+	if (!GetHitActors().IsEmpty())
+	{
+		ResetAllActors();
+	}
 }
 
 void UCollisionAttack::Cancel()
 {
-	SetIsActive(false);		// 攻撃無効化
+	SetIsActive(false); // 攻撃無効化
+}
+
+bool UCollisionAttack::IsHitCheckActiveNow(const float InElapsedTime) const
+{
+	// 点滅処理を行わないならtrueを返す
+	if (!BlinkingElement.bIsBlinking) { return true; }
+
+	// 経過時間をインターバルで割って、出力された数値が「奇数か偶数」かを取得
+	// 0.0f ~ 0.4fなら「0」, 0.5f ~ 1.0fなら「1」...
+	const int32 Phase = FMath::FloorToInt(InElapsedTime / BlinkingElement.BlinkInterval);
+	
+	// 偶数ならON
+	return (Phase % 2) == 0;
 }
 
 void UCollisionAttack::OnCollisionBeginOverlap(
@@ -44,7 +73,8 @@ void UCollisionAttack::OnCollisionBeginOverlap(
 )
 {
 	if (!OtherActor || !GetOwnerActor().IsValid()
-		|| OtherActor == GetOwnerActor()) {
+		|| OtherActor == GetOwnerActor())
+	{
 		return;
 	}
 
@@ -56,7 +86,7 @@ void UCollisionAttack::OnCollisionBeginOverlap(
 			interface->ApplyDamaged(GetFinalDamage());
 			interface->ApplyKnockBack(GetOwnerActor()->GetActorLocation());
 			AddHitActors(OtherActor);
-			
+
 			// 攻撃がHITした瞬間のデリゲートの発火
 			OnOverlapInDelegate.Broadcast(OtherActor);
 		}
@@ -71,14 +101,15 @@ void UCollisionAttack::OnCollisionEndOverlap(
 )
 {
 	if (!OtherActor || !GetOwnerActor().IsValid()
-		|| OtherActor == GetOwnerActor()) {
+		|| OtherActor == GetOwnerActor())
+	{
 		return;
 	}
-	
+
 	if (GetHitActors().Contains(OtherActor))
 	{
 		RemoveActor(OtherActor);
-		
+
 		// HIT判定から抜け出した瞬間のデリゲートの発火
 		OnOverlapOutDelegate.Broadcast(OtherActor);
 	}
