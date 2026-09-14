1455c7401ea69e8e63f77d4619223e819673f7af
1405586 <kd1405586@st.kobedenshi.ac.jp>
2026-09-14 11:36:01 +0900

    クリアタイムリザルト反映

--- Source/ProjectNull/System/Timer/FGameTimer.cpp
+++ Source/ProjectNull/System/Timer/FGameTimer.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "FGameTimer.h"
@@ -38,7 +38,6 @@ void FGameTimer::StopTimer()
 	if (World.IsValid())
 	{
 		World->GetTimerManager().ClearTimer(CountdownTimerHandle);
-		RemainingTime = 0.0f;
 	}
 }
 
